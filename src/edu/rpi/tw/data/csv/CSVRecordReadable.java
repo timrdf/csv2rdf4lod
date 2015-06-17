272fdaf105bfef1d2a483a8640155d299da20529
Tim L <timrdf@gmail.com>
2015-06-16 20:03:43 -0400

    radius 1 of main() (again)

---
+++ src/edu/rpi/tw/data/csv/CSVRecordReadable.java
@@ -0,0 +1,8 @@
+package edu.rpi.tw.data.csv;
+
+/**
+ * 
+ */
+public interface CSVRecordReadable {
+   public String getItem(String columnLabel);
+}
---
+++ src/edu/rpi/tw/data/csv/CSVRecordWriteable.java
@@ -0,0 +1,10 @@
+package edu.rpi.tw.data.csv;
+
+/**
+ * 
+ */
+public interface CSVRecordWriteable {
+   
+   public void addItem(int columnIndex, String value);
+
+}
