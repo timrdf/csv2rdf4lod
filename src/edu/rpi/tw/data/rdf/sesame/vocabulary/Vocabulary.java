251a4c7572a1826cbb3db8f168075313bf1a73c1
Tim L <timrdf@gmail.com>
2015-06-16 21:22:17 -0400

    radius 1 of main() (again)

---
+++ src/edu/rpi/tw/data/rdf/sesame/vocabulary/Vocabulary.java
@@ -0,0 +1,21 @@
+package edu.rpi.tw.data.rdf.sesame.vocabulary;
+
+/**
+ * 
+ */
+public interface Vocabulary {
+   
+   /**
+    * 
+    * @return
+    */
+   public String getBaseURI();
+   
+   /**
+    * 
+    * @return
+    */
+   public String getPrefix();
+  
+   //public Collection<Resource> getTerms();
+}
